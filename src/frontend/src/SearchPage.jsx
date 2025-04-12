8bd040b84853f836ab4304aa4b9632a3976bf62b
PranavKuramkoteSudhir <pranavkuramkotesudhir@gmail.com>
2025-04-12 16:07:10 -0400

    made changes

--- src/frontend/src/SearchPage.jsx
+++ src/frontend/src/SearchPage.jsx
@@ -27,7 +27,7 @@ const SearchPatient = () => {
 
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:8080/search-patient", {
+      const response = await fetch("https://term-deposit-backend-<hash>.a.run.app/search-patient", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
