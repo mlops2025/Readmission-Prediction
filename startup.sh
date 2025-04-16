b957e4a3e4474374b8fc8cf21c0387b712f3c677
Pranav Kuramkote Sudhir <139109363+PranavKuramkoteSudhir@users.noreply.github.com>
2025-04-16 01:38:24 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -51,6 +51,7 @@ mkdir -p dags logs plugins config data data/processed
 chown -R "${USER}:${USER}" dags logs plugins config data
 
 echo "[INFO] Starting Airflow containers..."
+export _PIP_ADDITIONAL_REQUIREMENTS="$(cat requirements.txt | tr '\n' ' ')"
 docker compose up airflow-init
 docker compose up -d
 
